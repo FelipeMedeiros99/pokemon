307014d6982d038b8f72ca74db7d0aec81c6e6f7
Felipe Medeiros Castello Branco <felipe.m.c.b.fm@gmail.com>
2023-10-20 15:35:57 -0300

    finalmente consegui fazer a barra

--- index.js
+++ index.js
@@ -18,8 +18,42 @@ const tiposPokemons = [
     'Voador',
     'Terrestre']
 
+const tipos = document.querySelector('.tipos')
+
+const elementoSelecionado = document.querySelector('.elementoSelecionado')
+
 // ----------- funções ------------//
+function criarNavBar(){
+    // cria a barra de navegação com as opções de tipos
+    let listaDePokemonsEmHTML = tiposPokemons.map(transformarEmHTML)
 
+    // adicionando os tipos ao html
+    listaDePokemonsEmHTML.forEach(function(elemento){
+        tipos.innerHTML += elemento
+    })
+}
+
+function transformarEmHTML(elemento){
+    return `
+    <p onclick=selecionaOpcao(this)>
+        ${elemento}
+    </p>`
+}
+
+function mostrarOcultarTipos(){
+    // ativado ao passar o mouse sobre os tipos
+    // faz parte do filtro
+    tipos.classList.toggle('ativado')
+
+}
+
+
+function selecionaOpcao(elemento){
+    elementoSelecionado.innerHTML = `
+    ${elemento.innerText} <ion-icon name="chevron-down-outline"></ion-icon>
+    `
+    mostrarOcultarTipos()
+
+}
 // -------- código principal ------//
-
-
+ criarNavBar()
